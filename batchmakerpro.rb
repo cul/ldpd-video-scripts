46ade9717f6d2f05f0d2b7576c04cf556a946afb
erik r <er2576@columbia.edu>
2016-02-04 17:08:57 -0500

    first commit

---
+++ batchmakerpro.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+################################################################################
+# batchmakerpro.rb
+# video analyzer, prober, and batch-list maker
+# wrapper script for ffprobe
+################################################################################
+
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require
+require 'csv'
+
+folder = ARGV[0] #"."
+vids = Dir.glob("#{folder}/**/*.{mp4,vob,wmv,avi,mpg,asf,mov}", File::FNM_CASEFOLD)
+CSV.open("./file.csv", "wb") do |csv|
+  csv << ["source_path", "duration"]
+  vids.each do | vid | 
+	fullpath = vid
+	puts fullpath
+    movie = FFMPEG::Movie.new(fullpath)
+    csv << [fullpath, movie.duration.to_s]
+  end
+end
