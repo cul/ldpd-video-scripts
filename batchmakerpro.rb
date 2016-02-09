e9e277cea6c1766e02beea16d4746a060fc7b47d
erik r <er2576@columbia.edu>
2016-02-09 16:17:57 -0500

    added more extensions. added valid. removed forced to_s. added to_f for frame_rate

--- batchmakerpro.rb
+++ batchmakerpro.rb
@@ -21,7 +21,7 @@ BM
 puts "Source dir: "+source_dir
 puts "CSV destination dir: "+dest_dir
 
-vids = Dir.glob("#{source_dir}/**/*.{mp4,vob,wmv,avi,mpg,asf,mov}", File::FNM_CASEFOLD)
+vids = Dir.glob("#{source_dir}/**/*.{mp4,vob,wmv,avi,mpg,mpeg,asf,mov,3gp,m4v,flv}", File::FNM_CASEFOLD)
 if vids.empty? 
 	puts "No vids found. Exiting." 
 	exit
@@ -30,10 +30,10 @@ else
 end
 
 CSV.open(dest_dir+"/batchfile.csv", "wb") do |csv|
-  csv << ["FILENAME", "DIRNAME", "DURATION (SECS)", "FPS", "RESOLUTION", "VIDEO CODEC", "AUDIO SAMPLE RATE (Hz)", "AUDIO CODEC", "BITRATE (KB/S)", "SIZE (BYTES)"]
+  csv << ["FILENAME", "DIRNAME", "DURATION (SECS)", "FPS", "RESOLUTION", "VIDEO CODEC", "AUDIO SAMPLE RATE (Hz)", "AUDIO CODEC", "BITRATE (KB/S)", "SIZE (BYTES)", "VALID"]
   vids.each do | vid | 
     movie = FFMPEG::Movie.new(vid)
-    csv << [File.basename(vid), File.dirname(vid), movie.duration.to_s, movie.frame_rate.to_s, movie.resolution.to_s, movie.video_codec.to_s, movie.audio_sample_rate.to_s, movie.audio_codec.to_s, movie.bitrate.to_s, movie.size.to_s]
+    csv << [File.basename(vid), File.dirname(vid), movie.duration, movie.frame_rate.to_f, movie.resolution, movie.video_codec, movie.audio_sample_rate, movie.audio_codec, movie.bitrate, movie.size, movie.valid?]
   end
 end
 
