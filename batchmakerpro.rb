550ae9a246ce753827facecb86f3955773d5c1e9
erik r <er2576@columbia.edu>
2016-02-09 12:56:23 -0500

    some cleanup. fattened ignores

--- batchmakerpro.rb
+++ batchmakerpro.rb
@@ -10,12 +10,31 @@ Bundler.require
 require 'csv'
 
 source_dir = ARGV[0] || "."
-vids = Dir.glob("#{source_dir}/**/*.{mp4,vob,wmv,avi,mpg,asf,mov}", File::FNM_CASEFOLD)
+dest_dir = ARGV[1] || "."
 
-CSV.open("./batchfile.csv", "wb") do |csv|
+puts <<BM
+ _______________
+< batchmakerpro >
+ ---------------
+BM
+
+puts "Source dir: "+source_dir
+puts "CSV destination dir: "+dest_dir
+
+vids = Dir.glob("#{source_dir}/**/*.{mp4,vob,wmv,avi,mpg,asf,mov}", File::FNM_CASEFOLD)
+if vids.empty? 
+	puts "No vids found. Exiting." 
+	exit
+else
+	puts "Found " + vids.length.to_s + " vids. Processing..."
+end
+
+CSV.open(dest_dir+"/batchfile.csv", "wb") do |csv|
   csv << ["FILENAME", "DIRNAME", "DURATION (SECS)", "FPS", "RESOLUTION", "VIDEO CODEC", "AUDIO SAMPLE RATE (Hz)", "AUDIO CODEC", "BITRATE (KB/S)", "SIZE (BYTES)"]
   vids.each do | vid | 
     movie = FFMPEG::Movie.new(vid)
     csv << [File.basename(vid), File.dirname(vid), movie.duration.to_s, movie.frame_rate.to_s, movie.resolution.to_s, movie.video_codec.to_s, movie.audio_sample_rate.to_s, movie.audio_codec.to_s, movie.bitrate.to_s, movie.size.to_s]
   end
 end
+
+puts "Done."
