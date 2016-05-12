10ada8271627a5cae55267cd1e4ff3cd3784b435
Benjamin Armintor <armintor@gmail.com>
2016-05-12 11:54:12 -0400

    iterate over a csv of source video info to transcode

--- transcoderx.rb
+++ transcoderx.rb
@@ -18,7 +18,6 @@ Bundler.require
 require 'csv'
 
 batchfile = ARGV[0] || "./batchfile.csv"
-full_path_outfile = ARGV[1] || "./transcodes"
 
 # preservation format we want: 
 #  Stream #0:0: Video: dvvideo, yuv411p, 720x480 [SAR 32:27 DAR 16:9], 28771 kb/s, 29.97 fps, 29.97 tbr, 29.97 tbn, 29.97 tbc
@@ -39,12 +38,48 @@ puts <<BM
  -------------
 BM
 
-movie = FFMPEG::Movie.new(batchfile)
+def help(msg = nil)
+  puts "ruby transcoderx.rb CSV_FILE SOURCE_ROOT DESTINATION_ROOT"
+  puts msg if msg
+  exit
+end
 
-puts "Creating Preservation Master"
-movie.transcode(full_path_outfile + ".dv", preservation_format) { |progress| print "\rPercent complete: " + (progress * 100).to_i.to_s + "%"  }
-puts "Done."
 
-puts "Creating Access Copy"
-movie.transcode(full_path_outfile + ".mp4") { |progress| print "\rPercent complete: " + (progress * 100).to_i.to_s + "%"  }
-puts "Done."
+
+help() unless ARGV[2]
+
+help("CSV_FILE \"#{batchfile}\" does not exist") unless File.exist?(batchfile)
+source_root = ARGV[1] || File.join(File.root, "Volumes","vid1")
+help("SOURCE_ROOT value of \"#{source_root}\" does not exist") unless File.exist?(source_root)
+destination_root = ARGV[2] || File.join(File.root, "Volumes","vid2")
+help("DESTINATION_ROOT value of \"#{destination_root}\" does not exist") unless File.exist?(destination_root)
+
+preservation_root = File.join(destination_root, 'preservation')
+access_root = File.join(destination_root, 'access')
+
+CSV.open(batchfile, "rb", headers: true) do |csv|
+  csv.each do |row|
+    next if row.header_row?
+    file_name = row[0]
+    dir_path = row[1]
+    dir_path = dir_path[1..-1] if dir_path =~ /^\//
+    video_source_path =  File.join(source_root, dir_path, file_name)
+    access_dir = File.join(access_root, dir_path, file_name)
+    FileUtils.makedirs(access_dir)
+    # then we use this as part of the output path
+    preservation_dir = File.join(preservation_root, dir_path, file_name)
+    FileUtils.makedirs(preservation_dir)
+    movie = FFMPEG::Movie.new(video_source_path)
+
+    base_file_name = File.basename(video_source_path, File.extname(video_source_path))
+    full_path_outfile = File.join(preservation_dir, "#{base_file_name}.dv")
+    puts "Creating Preservation Master"
+    movie.transcode(full_path_outfile, preservation_format) { |progress| print "\rPercent complete: " + (progress * 100).to_i.to_s + "%"  }
+    puts "Done with Preservation Master."
+
+    puts "Creating Access Copy"
+    full_path_outfile = File.join(access_dir, "#{base_file_name}.mp4")
+    movie.transcode(full_path_outfile, access_format) { |progress| print "\rPercent complete: " + (progress * 100).to_i.to_s + "%"  }
+    puts "Done with Access Copy."
+  end
+end
